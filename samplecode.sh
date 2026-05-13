035acc06f22c9f1f16cfc752d428c559b477e193
farooqkhanpfk <159903472+farooqkhanpfk@users.noreply.github.com>
2026-05-13 15:44:06 +0530

    Code was written in github

---
+++ samplecode.sh
@@ -0,0 +1,5 @@
+First create repo
+Second create file and commit with the message
+Third while committing select the option 2 and enter the branch name then propose changes 
+Fourth it will open a pull request write the description 
+Click Create Repositery
